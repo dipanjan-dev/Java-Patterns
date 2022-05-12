ec8d61a384d4ccc654344f6d58d179334d537807
Dipanjan Mukherjee <67435621+dipanjan-dev@users.noreply.github.com>
2022-05-12 10:20:24 +0530

    init

---
+++ HaifPyramid.java
@@ -0,0 +1,14 @@
+public class HaifPyramid{
+    public static void main(String[] args) {
+
+        int r = 5; //Row
+        for (int i=1;i<=r;i++) {
+            for (int j=1;j<=i;j++) {
+                System.out.print("*");   
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
