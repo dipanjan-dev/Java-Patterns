81d416b271714e9a95b9777869945ec4f30a37b3
Dipanjan Mukherjee <67435621+dipanjan-dev@users.noreply.github.com>
2022-05-12 10:17:13 +0530

    INIT

--- HoloRectangle.java
+++ HoloRectangle.java
@@ -2,16 +2,9 @@ public class HoloRectangle {
     public static void main(String[] args) {
 
         int r = 5; //Row
-        int c = 8; //Column
-
         for (int i=1;i<=r;i++) {
-            for (int j=1;j<=c;j++) {
-                if(i==1 || j==1 || i == r || j == c){
-                    System.out.print("*");
-                }
-                else {
-                    System.out.print(" ");
-                }
+            for (int j=1;j<=i;j++) {
+                System.out.print("*");   
             }
             System.out.println();
         }
