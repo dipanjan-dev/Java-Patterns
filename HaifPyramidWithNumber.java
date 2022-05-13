a89c923d40ab965ac2b9b6f45495590351e84934
Dipanjan Mukherjee <67435621+dipanjan-dev@users.noreply.github.com>
2022-05-13 09:07:00 +0530

    init

---
+++ HaifPyramidWithNumber.java
@@ -0,0 +1,12 @@
+public class HaifPyramidWithNumber {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = 1;i<=num;i++) {
+            for (int j = 1;j<=i;j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }    
+    
+    }
+}
